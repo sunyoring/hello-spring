1e49c63759495b8a6f76d8e71793eb45c74d7860
sunyoring <sunhyo67@gmail.com>
2021-10-05 17:02:47 +0900

    first commit

---
+++ src/test/java/com/sunyo/hellospring/HelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunyo.hellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
