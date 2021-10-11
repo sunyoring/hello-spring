d9701bbe1f6780ee855fc767deb13896e2638af8
sunyoring <sunhyo67@gmail.com>
2021-10-11 23:09:46 +0900

    ⚫ 회원조회

--- src/main/java/com/sunyo/hellospring/conroller/MemberController.java
+++ src/main/java/com/sunyo/hellospring/conroller/MemberController.java
@@ -4,8 +4,11 @@ import com.sunyo.hellospring.domain.Member;
 import com.sunyo.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 
 @Controller
@@ -38,5 +41,14 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members); //model에 리스트를 담아서 view에 넘긴다.
+        return "members/memberList";
+
     }
 }
